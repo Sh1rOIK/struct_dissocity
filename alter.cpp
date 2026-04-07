a44f210048dffd5a733d3e04afe02aa28a64aebe
Sh1rOIK <arsenharisov619@gmail.com>
2026-04-07 22:03:50 +0500

    Update alter.cpp

--- alter.cpp
+++ alter.cpp
@@ -1,5 +1,21 @@
 #include "alter.h"
 #include <string>
 #include <utility>
+#include <iostream>
 
 Alter::Alter() : name_alter("none"), function_alter("none"), callCounter(0) {}
+
+Alter::Alter(std::string name, std::string func) : 
+	name_alter(std::move(name)), function_alter(std::move(func)) {}
+
+void Alter::setAlters(std::string n, std::string f) {
+	name_alter = std::move(n);
+	function_alter = std::move(f);
+
+}
+
+void Alter::alter_info() {
+	callCounter++;
+	std::cout << "Call: " << callCounter << "| Name: "
+		<< name_alter << "| Function: " << function_alter << "\n";
+}
